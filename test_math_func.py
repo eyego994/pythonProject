c0ffc29f81e5fe74c6e9be7231f763c1e74bbc4a
parksanghyun <sanghyun.park@musinsa.com>
2021-06-02 16:17:56 +0900

    pytest 주석

--- test_math_func.py
+++ test_math_func.py
@@ -1,25 +1,25 @@
 import math_func
-import pytest
+# import pytest
 
-@pytest.mark.p1
+# @pytest.mark.p1
 def test_add():
     assert math_func.add(7,3) == 10
     assert math_func.add(7) == 9
     assert math_func.add(5) == 7
 
-@pytest.mark.p1
+# @pytest.mark.p1
 def test_product():
     assert math_func.product(5, 2) == 10
     assert math_func.product(5) == 10
 
-@pytest.mark.p2
+# @pytest.mark.p2
 def test_add_strings():
     result = math_func.add('hello ','world')
     assert result =='hello world'
     assert type(result) is str
     assert 'heldlo' not in result
 
-@pytest.mark.p2
+# @pytest.mark.p2
 def test_product_strings():
     assert math_func.product('hello ',3) == 'hello hello hello '
     result = math_func.product('hello ')
